b1f856ebbeb79929766bd50b1ef7ae7bb6cc7744
Muhammad Anas <111079610+muhammadanas-x@users.noreply.github.com>
2025-08-22 16:17:04 +0500

    Update next.config.mjs

--- lalala/next.config.mjs
+++ lalala/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    PINECONE_API_KEY: process.env.PINECONE_API_KEY,
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+  },
+};
 
 export default nextConfig;
